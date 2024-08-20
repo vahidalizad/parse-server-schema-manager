868f67e03df9965df90b7fe4ce9fd385452e1ab5
mahyarmadad <erfansam8891@yahoo.com>
2024-08-20 16:12:52 +0330

    add some test diffing

--- test/integration/management.spec.js
+++ test/integration/management.spec.js
@@ -2,12 +2,20 @@ import {describe, it} from 'mocha';
 import {expect} from 'chai';
 import Parse from 'parse/node';
 
-describe('Test Diffs', function () {
-  it('test diffing of whole schema', async function () {});
+describe('Test Manage Schema', function () {
+  it('test get field of a schema class ', async function () {});
 
-  it('test commit', async function () {});
+  it('test add field to schema class', async function () {});
 
-  it('test remove', async function () {});
+  it('test not add field to schema class with commit false', async function () {});
 
-  it('test purge and remove', async function () {});
+  it('test change field type in field class', async function () {});
+
+  it('test remove empty field in class with only remove', async function () {});
+
+  it('test remove field in class', async function () {});
+
+  it('test not to remove empty field in class with remove false', async function () {});
+
+  it('test not to remove field in class with purge false', async function () {});
 });
