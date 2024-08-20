592fe2c92516072a6fb5f8789586b477f137a8af
mahyarmadad <erfansam8891@yahoo.com>
2024-08-20 17:17:16 +0330

    add a test

--- test/integration/management.spec.js
+++ test/integration/management.spec.js
@@ -1,11 +1,68 @@
 import {describe, it} from 'mocha';
-import {expect} from 'chai';
-import Parse from 'parse/node';
+import testSchemas from '../assets/schema.json';
+import {manageSchema} from 'src';
+
+const schema = testSchemas[0];
+const actionParts = {fields: true};
+const schemaOptions = {};
 
 describe('Test Manage Schema', function () {
-  it('test get field of a schema class ', async function () {});
+  it('test diffs field of a schema class', async function () {
+    const actions = {commit: false};
+    const result = await manageSchema(
+      schema,
+      actions,
+      actionParts,
+      schemaOptions
+    );
+    expect(result).to.deep.equal([
+      {
+        className: '_User',
+        fields: {
+          ACL: {type: 'ACL'},
+          createdAt: {type: 'Date'},
+          email: {type: 'String'},
+          emailVerified: {type: 'Boolean'},
+          objectId: {type: 'String'},
+          password: {type: 'String'},
+          updatedAt: {type: 'Date'},
+          username: {type: 'String'},
+        },
+      },
+    ]);
+  });
 
-  it('test add field to schema class', async function () {});
+  it('test add field to schema class', async function () {
+    const actions = {commit: true};
+    const newSchema = {
+      ...schema,
+      fields: {...schema.fields, test: {type: 'String'}},
+    };
+    const result = await manageSchema(
+      newSchema,
+      actions,
+      actionParts,
+      schemaOptions
+    );
+    expect(result).to.deep.equal([
+      {
+        className: '_User',
+        fields: {
+          ACL: {type: 'ACL'},
+          createdAt: {type: 'Date'},
+          email: {type: 'String'},
+          emailVerified: {type: 'Boolean'},
+          objectId: {type: 'String'},
+          password: {type: 'String'},
+          updatedAt: {type: 'Date'},
+          username: {type: 'String'},
+          test: {type: 'String'},
+        },
+      },
+    ]);
+  });
+
+  it('test not add exist field to schema class with commit true', async function () {});
 
   it('test not add field to schema class with commit false', async function () {});
 
