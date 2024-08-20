22d2dedc4555419a9a3ff5c63f1db989ff1e0438
vahid <azizalizad@gmail.com>
2024-08-20 15:38:51 +0330

    fix tests names

---
+++ test/integration/management.spec.js
@@ -0,0 +1,13 @@
+import {describe, it} from 'mocha';
+import {expect} from 'chai';
+import Parse from 'parse/node';
+
+describe('Test Diffs', function () {
+  it('test diffing of whole schema', async function () {});
+
+  it('test commit', async function () {});
+
+  it('test remove', async function () {});
+
+  it('test purge and remove', async function () {});
+});
