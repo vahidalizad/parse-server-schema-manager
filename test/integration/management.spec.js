34fe9c41619ada6ed3dec8ab8a38f6f4b1e7da7b
vahid <azizalizad@gmail.com>
2024-08-21 16:30:06 +0330

    change logic of test

--- test/integration/management.spec.js
+++ test/integration/management.spec.js
@@ -1,63 +1,65 @@
 import {manageSchema} from '@Functions/schema';
-import {createSchema} from '@test/helper';
 import {expect} from 'chai';
 import {describe, it} from 'mocha';
+import TestSchema from '../assets/test-schema.json';
+import DefaultSchema from '../assets/default-schema.json';
 
-const actionParts = {fields: true};
-const schemaOptions = {};
+const schemaOptions = {
+  ignoreClasses: ['_Session', '_User', '_Role'],
+  ignoreAttributes: [],
+};
+
+const cleanSchema = async () => {
+  try {
+    await manageSchema(
+      DefaultSchema,
+      {commit: true, purge: true, remove: true},
+      {fields: true, indexes: true, classLevelPermissions: true},
+      {
+        ignoreClasses: ['_Session'],
+        ignoreAttributes: [],
+      }
+    );
+  } catch (error) {
+    console.error('Default Schema: ', error);
+  }
+};
 
 describe('Test Manage Schema', function () {
-  let testSchema = null;
-  this.beforeAll(async function () {
-    testSchema = await createSchema();
+  this.beforeEach(async function () {
+    await cleanSchema();
   });
 
-  it('test diffs field of a schema class', async function () {
-    const actions = {commit: false};
-    const result = await manageSchema(
-      [testSchema],
-      actions,
-      actionParts,
-      schemaOptions
-    );
-    console.log('result', result);
-    debugger;
-    return expect(result).to.deep.equal([
-      {
-        className: 'Test',
-        _fields: {
-          age: {type: 'String'},
-          name: {type: 'String'},
-        },
-      },
-    ]);
+  this.afterAll(async function () {
+    await cleanSchema();
   });
 
-  it('test add field to schema class', async function () {
-    const actions = {commit: true};
-    const newSchema = {
-      ...testSchema,
-      _fields: {...testSchema._fields, test: {type: 'String'}},
-    };
-    debugger;
+  it('test add / remove class from schema', async function () {
     const result = await manageSchema(
-      [newSchema],
-      actions,
-      actionParts,
+      TestSchema,
+      {commit: true},
+      {fields: true},
       schemaOptions
     );
+    expect(result).to.deep.equal({
+      add: {Test: TestSchema[0]},
+      log: 'Schema synced!',
+    });
+    const result2 = await manageSchema(
+      TestSchema,
+      {commit: true},
+      {fields: true},
+      schemaOptions
+    );
+    expect(result2).to.deep.equal({
+      log: 'Schema synced!',
+    });
     debugger;
-    return expect(result).to.deep.equal([
-      {
-        className: 'Test',
-        _fields: {
-          age: {type: 'String'},
-          name: {type: 'String'},
-          test: {type: 'String'},
-        },
-      },
-    ]);
   });
+
+  it.skip('test diffs field of a schema class', async function () {});
+
+  it.skip('test add field to schema class', async function () {});
 
   it.skip('test not add exist field to schema class with commit true', async function () {});
 
@@ -72,12 +74,4 @@ describe('Test Manage Schema', function () {
   it.skip('test not to remove empty field in class with remove false', async function () {});
 
   it.skip('test not to remove field in class with purge false', async function () {});
-
-  // this.afterAll(async function () {
-  //   try {
-  //     await testSchema.purge();
-  //   } catch (error) {
-  //     console.error('Delete Schema: ', error);
-  //   }
-  // });
 });
