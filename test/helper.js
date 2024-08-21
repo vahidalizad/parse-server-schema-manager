2c51351ba375b2ed420eab5f187a0bac2ace6072
mahyarmadad <erfansam8891@yahoo.com>
2024-08-21 13:54:54 +0330

    try to run test but get Schema not found

--- test/helper.js
+++ test/helper.js
@@ -1,1 +1,53 @@
+export async function createSchema(className) {
+  try {
+    const schema = new Parse.Schema(className || 'Test');
+    schema.addString('name');
+    schema.addNumber('age');
+    return schema;
+  } catch (error) {
+    console.error('Failed to create schema:', error);
+    throw error;
+  }
+}
+export async function createUserSchema() {
+  const UserSchema = new Parse.Schema('_User');
+  UserSchema.add('ACL', new Parse.ACL());
+  UserSchema.add('createdAt', new Date());
+  UserSchema.add('email', new Parse.String());
+  UserSchema.add('emailVerified', new Parse.Boolean());
+  UserSchema.add('objectId', new Parse.String());
+  UserSchema.add('password', new Parse.String());
+  UserSchema.add('updatedAt', new Date());
+  UserSchema.add('username', new Parse.String());
+  UserSchema.add('authData', new Parse.Object());
 
+  UserSchema.indexes().addIndex('email', true); // case_insensitive_email
+  UserSchema.indexes().addIndex('username', true); // case_insensitive_username
+
+  try {
+    await UserSchema.save();
+    return UserSchema;
+  } catch (error) {
+    console.error('Failed to create/update User schema:', error);
+  }
+}
+
+export async function createRoleSchema() {
+  const RoleSchema = new Parse.Schema('_Role');
+  RoleSchema.add('objectId', new Parse.String());
+  RoleSchema.add('createdAt', new Date());
+  RoleSchema.add('updatedAt', new Date());
+  RoleSchema.add('ACL', new Parse.ACL());
+  RoleSchema.add('name', new Parse.String());
+  RoleSchema.add('users', new Parse.Relation({targetClass: '_User'}));
+  RoleSchema.add('roles', new Parse.Relation({targetClass: '_Role'}));
+
+  RoleSchema.indexes().addIndex('name', true); // name_1
+
+  try {
+    await RoleSchema.save();
+    return RoleSchema;
+  } catch (error) {
+    console.error('Failed to create/update Role schema:', error);
+  }
+}
--- test/integration/management.spec.js
+++ test/integration/management.spec.js
@@ -1,34 +1,33 @@
-import {describe, it} from 'mocha';
-import testSchemas from '../assets/schema.json';
 import {manageSchema} from '@Functions/schema';
+import {createSchema} from '@test/helper';
 import {expect} from 'chai';
+import {describe, it} from 'mocha';
 
-const schema = testSchemas[0];
 const actionParts = {fields: true};
 const schemaOptions = {};
 
 describe('Test Manage Schema', function () {
+  let testSchema = null;
+  this.beforeAll(async function () {
+    testSchema = await createSchema();
+  });
+
   it('test diffs field of a schema class', async function () {
     const actions = {commit: false};
     const result = await manageSchema(
-      [schema],
+      [testSchema],
       actions,
       actionParts,
       schemaOptions
     );
+    console.log('result', result);
     debugger;
-    expect(result).to.deep.equal([
+    return expect(result).to.deep.equal([
       {
-        className: '_User',
-        fields: {
-          ACL: {type: 'ACL'},
-          createdAt: {type: 'Date'},
-          email: {type: 'String'},
-          emailVerified: {type: 'Boolean'},
-          objectId: {type: 'String'},
-          password: {type: 'String'},
-          updatedAt: {type: 'Date'},
-          username: {type: 'String'},
+        className: 'Test',
+        _fields: {
+          age: {type: 'String'},
+          name: {type: 'String'},
         },
       },
     ]);
@@ -37,44 +36,48 @@ describe('Test Manage Schema', function () {
   it('test add field to schema class', async function () {
     const actions = {commit: true};
     const newSchema = {
-      ...schema,
-      fields: {...schema.fields, test: {type: 'String'}},
+      ...testSchema,
+      _fields: {...testSchema._fields, test: {type: 'String'}},
     };
+    debugger;
     const result = await manageSchema(
-      newSchema,
+      [newSchema],
       actions,
       actionParts,
       schemaOptions
     );
-    expect(result).to.deep.equal([
+    debugger;
+    return expect(result).to.deep.equal([
       {
-        className: '_User',
-        fields: {
-          ACL: {type: 'ACL'},
-          createdAt: {type: 'Date'},
-          email: {type: 'String'},
-          emailVerified: {type: 'Boolean'},
-          objectId: {type: 'String'},
-          password: {type: 'String'},
-          updatedAt: {type: 'Date'},
-          username: {type: 'String'},
+        className: 'Test',
+        _fields: {
+          age: {type: 'String'},
+          name: {type: 'String'},
           test: {type: 'String'},
         },
       },
     ]);
   });
 
-  it('test not add exist field to schema class with commit true', async function () {});
+  it.skip('test not add exist field to schema class with commit true', async function () {});
 
-  it('test not add field to schema class with commit false', async function () {});
+  it.skip('test not add field to schema class with commit false', async function () {});
 
-  it('test change field type in field class', async function () {});
+  it.skip('test change field type in field class', async function () {});
 
-  it('test remove empty field in class with only remove', async function () {});
+  it.skip('test remove empty field in class with only remove', async function () {});
 
-  it('test remove field in class', async function () {});
+  it.skip('test remove field in class', async function () {});
 
-  it('test not to remove empty field in class with remove false', async function () {});
+  it.skip('test not to remove empty field in class with remove false', async function () {});
 
-  it('test not to remove field in class with purge false', async function () {});
+  it.skip('test not to remove field in class with purge false', async function () {});
+
+  // this.afterAll(async function () {
+  //   try {
+  //     await testSchema.purge();
+  //   } catch (error) {
+  //     console.error('Delete Schema: ', error);
+  //   }
+  // });
 });
